ae5fd3039e763e0981848e9020dd33a7629b699d
Jean-Hadrien DAMAY <jh.damay@gmail.com>
2020-01-27 16:03:44 +0100

    yes

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,6 +1,9 @@
 package project;
 
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.formsfx.model.structure.Group;
@@ -9,6 +12,9 @@ import com.dlsc.formsfx.view.renderer.FormRenderer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -25,13 +31,18 @@ import javafx.stage.Stage;
 public class App extends Application
 {
 	
+	static private Path path;
+	
     @Override
     public void start(Stage stage) {
+    	stage.setTitle("Contact Manager");
+    	stage.getIcons().add(new Image(path.toUri()+"resources/icon.png"));
+    	
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         
         Pane root = new Pane();
-
+        Pane root2 = new Pane();
         
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         root.getChildren().add(l);
@@ -44,12 +55,18 @@ public class App extends Application
                                 .label("Username"),
                         Field.ofStringType(y)
                                 .label("Password")
-                                .required("This field can’t be empty")
+                                .required("This field can't be empty")
                 )
         ).title("Login");
         root.getChildren().add(new FormRenderer(loginForm));
         
-        Scene scene = new Scene(new StackPane(root), 1280, 720);
+        ListView<String> listView = new ListView<String>();
+        listView.getItems().add("xd");
+        listView.getItems().add("xd");
+        root2.getChildren().add(listView);
+        
+        
+        Scene scene = new Scene(new HBox(root,root2), 1280, 720);
         stage.setScene(scene);
         stage.show();
         
@@ -57,7 +74,10 @@ public class App extends Application
     
     public static void main( String[] args )
     {
+    	
         System.out.println( "Java2 class final project! " + System.getProperty("java.version") );
+    	path = FileSystems.getDefault().getPath("");
+    	System.out.println(path.toUri());
     	launch();
     }
 }
