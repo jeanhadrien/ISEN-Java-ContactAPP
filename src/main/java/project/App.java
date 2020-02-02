f1f12d2ccce60c1c16e770d8c92783054f7e95bb
jeanhadrien <40248338+jeanhadrien@users.noreply.github.com>
2020-02-02 16:38:03 +0100

    update

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -2,6 +2,8 @@ package project;
 
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
@@ -12,73 +14,115 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- *  Contact application project for java2 class.
- *  Origin created on 20/01/2020
- *  
- *  Jean-Hadrien Damay
- *  Lionel Mbanza 
- *  
- *  
+ * Contact application project for java2 class. Origin created on 20/01/2020
+ * 
+ * Jean-Hadrien Damay Lionel Mbanza
+ * 
+ * 
  */
-public class App extends Application
-{
-	
-	static private Path path;
-	
-    @Override
-    public void start(Stage stage) {
-    	stage.setTitle("Contact Manager");
-    	stage.getIcons().add(new Image(path.toUri()+"resources/icon.png"));
-    	
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        
-        Pane root = new Pane();
-        Pane root2 = new Pane();
-        
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        root.getChildren().add(l);
+public class App extends Application {
 
-        String x = null,y = null;
-        
-        /*
-        Form loginForm = Form.of(
-                Group.of(
-                        Field.ofStringType(x)
-                                .label("Username"),
-                        Field.ofStringType(y)
-                                .label("Password")
-                                .required("This field can't be empty")
-                )
-        ).title("Login");
-        root.getChildren().add(new FormRenderer(loginForm)); */
-        
-        ListView<String> listView = new ListView<String>();
-        listView.getItems().add("xd");
-        listView.getItems().add("xd");
-        root2.getChildren().add(listView);
-        
-        
-        Scene scene = new Scene(new HBox(root,root2), 1280, 720);
-        stage.setScene(scene);
-        stage.show();
-        
-    }
-        
-    public static void main( String[] args )
-    {
-    	
-        System.out.println( "Java2 class final project! " + System.getProperty("java.version") );
-    	path = FileSystems.getDefault().getPath("");
-    	System.out.println(path.toAbsolutePath());
-    	    	    	
-    	//launch();
-    }
+	static private Path path;
+	static boolean debug = true;
+
+	@Override
+	public void start(Stage mainStage) {
+
+		mainStage.setTitle("Contact Manager");
+		mainStage.getIcons().add(new Image(path.toUri() + "resources/icon.png"));
+		Pane root = new Pane();
+
+		String javaVersion = System.getProperty("java.version");
+		String javafxVersion = System.getProperty("javafx.version");
+
+		Pane root2 = new Pane();
+
+		Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+		root.getChildren().add(l);
+
+		String x = null, y = null;
+
+		/*
+		 * Form loginForm = Form.of( Group.of( Field.ofStringType(x) .label("Username"),
+		 * Field.ofStringType(y) .label("Password")
+		 * .required("This field can't be empty") ) ).title("Login");
+		 * root.getChildren().add(new FormRenderer(loginForm));
+		 */
+
+		ListView<String> listView = new ListView<String>();
+		listView.getItems().add("xd");
+		listView.getItems().add("xd");
+		root2.getChildren().add(listView);
+
+		Scene scene = new Scene(new HBox(root, root2), 1280, 720);
+
+		mainStage.setScene(scene);
+		mainStage.show();
+
+		App.Logging.init();
+		print("XD");
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Java2 class final project! " + System.getProperty("java.version"));
+		path = FileSystems.getDefault().getPath("");
+		System.out.println(path.toAbsolutePath());
+
+		launch();
+	}
+
+	public static void print(String s) {
+		App.Logging.add(s);
+	}
+
+	private static class Logging {
+		static private List<String> list;
+		static private int size = 100;
+		static private Stage stage;
+		static private Scene scene;
+		static private Pane pane;
+		static private ListView<String> view;
+
+		public static void init() {
+			if (!App.debug)
+				return;
+			App.Logging.list = new ArrayList<String>();
+			App.Logging.stage = new Stage();
+			App.Logging.pane = new Pane();
+			App.Logging.view = new ListView<String>();
+			App.Logging.pane.getChildren().add(view);
+			App.Logging.scene = new Scene(new VBox(App.Logging.pane), 248, 400);
+			App.Logging.stage.setTitle("Console");
+			App.Logging.stage.setScene(App.Logging.scene);
+			App.Logging.stage.show();
+		}
+
+		private static void add(String s) {
+			if (!App.debug)
+				return;
+			App.Logging.view.getItems().add(0, s);
+			if (App.Logging.view.getItems().size() > App.Logging.size) {
+				App.Logging.view.getItems().remove(App.Logging.size);
+			}
+			return;
+		}
+
+		private static void clear() {
+			if (!App.debug)
+				return;
+			App.Logging.view.getItems().clear();
+		}
+
+	}
+
 }
--- src/main/java/project/Contact.java
+++ src/main/java/project/Contact.java
@@ -1,112 +1,201 @@
 package project;
 
 public class Contact {
-	
+
 	static class Regex {
 		static String Phone = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$";
 		static String Email = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\\\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\\\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 		static String Basic = "^[a-zA-Z0-9\\ \\-\\'À-ú]+$";
 		static String Word = "^[a-zA-ZÀ-ú]+$";
 	}
-	
-	Contact(Database db){
+
+	Contact(Database db) {
 		this.db = db;
 	}
-
+	
 	private Database db;
-	private int ID;
-	private String lastName,firstName,nickName;
-	private Date birthDate;
-		
-	public class Address{
-		private String street, city, state, postal, country;
-		
-		Address(String street, String city, String state, String postal, String country) throws IllegalArgumentException{
-			this.set(street, city, state, postal, country);
+
+	public class Name {
+		private String last, first, nick;
+
+		public String getLast() {
+			return last;
+		}
+
+		public String getFirst() {
+			return first;
+		}
+
+		public String getNick() {
+			return nick;
+		}
+
+		public void setLast(String last) {
+			if (last.matches(Regex.Basic) && last.length()<= db.length_Name) {
+				this.last = last;
+			}
+			else {
+				App.print("Invalid LastName");
+				throw new IllegalArgumentException("Invalid LastName");
+			}
+		}
+
+		public void setFirst(String first) {
+			if (first.matches(Regex.Basic) && first.length()<= db.length_Name) {
+				this.first = first;
+			}
+			else {
+				App.print("Invalid FirstName");
+				throw new IllegalArgumentException("Invalid FirstName");
+			}
+		}
+
+		public void setNick(String nick) {
+			if (nick.matches(Regex.Basic) && nick.length()<= db.length_Name) {
+				this.nick = nick;
+			}
+			else {
+				App.print("Invalid NickName");
+				throw new IllegalArgumentException("Invalid NickName");
+			}
 		}
 		
+		@Override
+		public String toString() {
+			return "".concat(this.first).concat(" '").concat(this.nick).concat("' ").concat(this.last);
+		}		
+
+	}
+
+	public class Birth {
+		private int day = 0, month = 0, year = 0;
+
+		Birth(String year, String month, String day) throws NumberFormatException, IllegalArgumentException {
+			int intDay = 0, intYear = 0, intMonth = 0;
+			try {
+				intDay = Integer.parseInt(day.trim());
+				intYear = Integer.parseInt(year.trim());
+				intMonth = Integer.parseInt(month.trim());
+
+			} catch (NumberFormatException nfe) {
+				App.print("Can't convert Date strings to ints");
+				throw new NumberFormatException("Can't convert Date strings to ints");
+			} finally {
+				this.set(intYear, intMonth, intDay);
+			}
+		}
+
 		String get() {
 			return this.toString();
 		}
-		
-		void set(String street, String city, String state, String postal, String country) throws IllegalArgumentException {
+
+		public void set(int year, int month, int day) {
+			if (this.day > 30 || this.day <= 0 || this.month > 12 || this.month <= 0 || this.year > 2020
+					|| this.year <= 1900 || (this.month == 2 && this.day > 28)) {
+				App.print("Invalid date values");
+				throw new IllegalArgumentException("Invalid date values");
+			} else {
+				this.day = day;
+				this.year = year;
+				this.month = month;
+			}
+		}
+
+		@Override
+		public String toString() {
+			return "".concat(Integer.toString(this.year)).concat("-").concat(Integer.toString(this.month)).concat("-")
+					.concat(Integer.toString(this.day));
+		}
+
+	}
+
+	public class Address {
+		private String street, city, state, postal, country;
+
+		Address(String street, String city, String state, String postal, String country)
+				throws IllegalArgumentException {
+			this.set(street, city, state, postal, country);
+		}
+
+		String get() {
+			return this.toString();
+		}
+
+		public void set(String street, String city, String state, String postal, String country)
+				throws IllegalArgumentException {
 			if (street.matches(Regex.Basic) && city.matches(Regex.Word) && state.matches(Regex.Word)
-					 && postal.matches(Regex.Basic) && country.matches(Regex.Word)
-					 && street.length()+city.length() + state.length() + postal.length() + country.length() <= db.length_Address){
+					&& postal.matches(Regex.Basic) && country.matches(Regex.Word) && street.length() + city.length()
+							+ state.length() + postal.length() + country.length() <= db.length_Address) {
 				this.street = street;
 				this.city = city;
 				this.state = state;
 				this.postal = postal;
-				this.country = country;				
+				this.country = country;
+			} else {
+				App.print("Invalid Address");
+				throw new IllegalArgumentException("Invalid Address");
 			}
-			else { throw new IllegalArgumentException("Failed to match regex.");}	
 		}
-		
+
 		@Override
 		public String toString() {
-			return this.street + "\n"
-					+ this.city + ", " + this.state + " "+ this.postal + "\n"
-					 + this.country;
+			return this.street + "\n" + this.city + ", " + this.state + " " + this.postal + "\n" + this.country;
 		}
-		
+
 	}
-	
+
 	class Phone {
-		private String number;   				
-		
-		Phone(String s) throws IllegalArgumentException{
+		private String number;
+
+		Phone(String s) throws IllegalArgumentException {
 			this.set(s);
 		}
-		
+
 		String get() {
 			return this.number;
 		}
-		
+
 		void set(String s) throws IllegalArgumentException {
-			if (s.matches(Regex.Phone) && s.length()<=db.length_Phone){
+			if (s.matches(Regex.Phone) && s.length() <= db.length_Phone) {
 				this.number = s;
+			} else {
+				App.print("Invalid Phone");
+				throw new IllegalArgumentException("Invalid Phone");
 			}
-			else { throw new IllegalArgumentException("Failed to match regex.");}	
 		}
 
-		
 		@Override
 		public String toString() {
 			return this.number;
 		}
-		
+
 	}
-	
-	class Email{
+
+	class Email {
 		private String email = "null";
-		
+
 		Email(String s) throws IllegalArgumentException {
 			this.set(s);
 		}
-		
+
 		String get() {
 			return this.email;
 		}
-		
+
 		void set(String s) throws IllegalArgumentException {
-			if (s.matches(Regex.Email) && s.length()<=db.length_Email){
+			if (s.matches(Regex.Email) && s.length() <= db.length_Email) {
 				this.email = s;
+			} else {
+				App.print("Invalid Email");
+				throw new IllegalArgumentException("Invalid Email");
 			}
-			else { throw new IllegalArgumentException("Failed to match regex.");}	
 		}
-		
+
 		@Override
 		public String toString() {
 			return this.email;
 		}
-		
+
 	}
-	
-		
-	
-	
-	class Date{
-		
-	}
-	
+
 }
