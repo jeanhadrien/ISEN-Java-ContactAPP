e7d72ca7345ef6fc2be6e43c67f6261f11a87154
jeanhadrien <40248338+jeanhadrien@users.noreply.github.com>
2020-02-03 13:18:42 +0100

    siuzherf

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -3,23 +3,36 @@ package project;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.List;
 
 import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.formsfx.model.structure.Group;
 import com.dlsc.formsfx.view.renderer.FormRenderer;
+import com.sun.javafx.scene.control.IntegerField;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -31,7 +44,7 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-	static private Path path;
+	static protected Path path;
 	static boolean debug = true;
 
 	@Override
@@ -60,9 +73,12 @@ public class App extends Application {
 		mainStage.show();
 
 		App.Logging.init();
-		print("XD");
+		App.SQLSetupConnectionWindow.init();
+		App.print("XD");
+		Sql.init();
 		
-		Sql.testStatement("SELECT * FROM contactapp.contact;");
+	
+		//Sql.executeLiteralStatement("SELECT * FROM contactapp.contact;");
 
 	}
 
@@ -87,7 +103,7 @@ public class App extends Application {
 		App.Logging.add(s);
 	}
 
-	public static class Logging {
+	private static class Logging {
 		static private List<String> list;
 		static private int size = 100;
 		static private Stage stage;
@@ -127,20 +143,112 @@ public class App extends Application {
 
 	}
 
-	private static class MySQLConnection{
+	private static class SQLSetupConnectionWindow{
 		static private Stage stage;
 		static private Scene scene;
 		static private Pane pane;
+
 		
 		public static void init() {
-			App.Logging.stage = new Stage();
-			App.Logging.pane = new Pane();
-			App.Logging.scene = new Scene(new VBox(App.Logging.pane), 248, 400);
-			App.Logging.stage.setTitle("Setup SQL Connection");
-			App.Logging.stage.setScene(App.Logging.scene);
-			App.Logging.stage.show();
+			stage = new Stage();
+			pane = new Pane();
+		    // Instantiate a new Grid Pane
+		    GridPane gridPane = new GridPane();
+
+		    // Position the pane at the center of the screen, both vertically and horizontally
+		    gridPane.setAlignment(Pos.CENTER);
+
+		    // Set a padding of 20px on each side
+		    gridPane.setPadding(new Insets(40, 40, 40, 40));
+
+		    // Set the horizontal gap between columns
+		    gridPane.setHgap(10);
+
+		    // Set the vertical gap between rows
+		    gridPane.setVgap(10);
+
+		    // Add Column Constraints
+
+		    // columnOneConstraints will be applied to all the nodes placed in column one.
+		    ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+		    columnOneConstraints.setHalignment(HPos.RIGHT);
+
+		    // columnTwoConstraints will be applied to all the nodes placed in column two.
+		    ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+		    columnTwoConstrains.setHgrow(Priority.ALWAYS);
+
+		    gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+		    		    
+		    // Add Name Label
+		    Label serverLabel = new Label("Server");
+		    gridPane.add(serverLabel, 0,0);
+		    TextField serverField = new TextField();
+		    //serverField.setText(prefDict.get("ServerName").toString());
+		    serverField.setPrefHeight(40);
+		    gridPane.add(serverField, 1,0);
+
+		    Label portLabel = new Label("Port");
+		    gridPane.add(portLabel, 0, 1);
+		    TextField portField = new TextField();
+		    portField.setPrefHeight(40);
+		    gridPane.add(portField, 1, 1);
+
+		    Label databaseLabel = new Label("Database");
+		    gridPane.add(databaseLabel, 0, 2);
+		    TextField databaseField = new TextField();
+		    databaseField.setPrefHeight(40);
+		    gridPane.add(databaseField, 1, 2);
+		    
+		    Label userLabel = new Label("User");
+		    gridPane.add(userLabel, 0, 3);
+		    TextField userField = new TextField();
+		    userField.setPrefHeight(40);
+		    gridPane.add(userField, 1, 3);
+		    
+		    Label passwordLabel = new Label("Password");
+		    gridPane.add(passwordLabel, 0, 4);
+		    PasswordField passwordField = new PasswordField();
+		    passwordField.setPrefHeight(40);
+		    gridPane.add(passwordField, 1, 4);
+
+		    // Add Submit Button
+		    Button submitButton = new Button("Submit");
+		    submitButton.setPrefHeight(40);
+		    submitButton.setDefaultButton(true);
+		    submitButton.setPrefWidth(100);
+		    gridPane.add(submitButton, 0, 5, 2, 1);
+		    GridPane.setHalignment(submitButton, HPos.CENTER);
+		    GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
+		    
+			scene = new Scene(gridPane, 500, 400);
+			stage.setTitle("Setup SQL Connection");
+			stage.setScene(scene);
+			stage.show();
+			
+			
 		}
+		
+		
 		
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/project/Sql.java
+++ src/main/java/project/Sql.java
@@ -1,18 +1,60 @@
 package project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLSyntaxErrorException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 import javax.sql.DataSource;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class Sql {
 
 	private static MysqlDataSource dataSource;
+	private static Preferences prefs;
 
+	public static void init() {
+		JSONObject obj = new JSONObject();
+		JSONParser pars = new JSONParser();
+		JSONObject json = null;
+		
+		Path mypath = App.path.resolve("resources/sqlconfig.json").toAbsolutePath();
+		
+		try(FileReader reader = new FileReader(mypath.toString())){
+			json = (JSONObject) pars.parse(reader);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		System.out.println(json.get("Username"));
+	}
+	
 	public static DataSource getDataSource() throws SQLException {
 		if (dataSource == null) {
 			dataSource = new MysqlDataSource();
@@ -26,23 +68,50 @@ public class Sql {
 		return dataSource;
 	}
 	
-	public static ResultSet testStatement(String s) {
+	
+	private static void setPrefs() {
+		
+	
+		/*prefs.put("ServerName", dataSource.getServerName());
+		prefs.putInt("Port", dataSource.getPort());
+		prefs.put("DatabaseName", dataSource.getDatabaseName());
+		prefs.put("Username", dataSource.getUser());*/
+		
+	}
+
+	public static void getPrefs() {
+		
+		/*
+		Dictionary<String, Object> dict = new Hashtable<String, Object>();
+		dict.put("tst","x");
+
+		String x = prefs.get("Username", "root");
+		dict.put("Username",prefs.get("Username", "root"));
+		dict.put("ServerName",prefs.get("ServerName", "localhost"));
+		dict.put("DatabaseName",prefs.get("DatabaseName", "contactapp"));
+		dict.put("Port",prefs.getInt("Port", 3306));
+		return dict;*/
+	}
+	
+	
+	
+	public static ResultSet executeLiteralStatement(String s) {
 		try (Connection connection = Sql.getDataSource().getConnection()) {
 			
 			try(PreparedStatement statement = connection.prepareStatement(s)){
 				//statement.setString(parameterIndex, x);
 				
 				try(ResultSet results = statement.executeQuery()){
-					App.print(":))");
+					App.print("SUCCESS : "+s);
 					return results;
 				}
-			}
+			}		
 			
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			App.print("Error connecting to database");
 			e.printStackTrace();
-		}
+		} 
 		return null;
 
 	}
