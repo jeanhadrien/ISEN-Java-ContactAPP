6b9214e6f2aa4dae0f7dc079fc98caf3700bfa74
jeanhadrien <jh.damay@gmail.com>
2020-02-28 21:37:09 +0100

    reset

--- target/classes/createTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `contactapp`.`contact` (
-  `idcontact` INT NOT NULL AUTO_INCREMENT,
-  `lastname` VARCHAR(45) NOT NULL,
-  `firstname` VARCHAR(45) NOT NULL,
-  `phone` VARCHAR(15) NOT NULL,
-  `address` VARCHAR(200) NULL,
-  `email` VARCHAR(150) NULL,
-  `birth` DATE NULL,
-  PRIMARY KEY (`idcontact`));
